09fd9e1625e8b7eabeaf246f4a55fcef2959af3a
蜗牛小安 <woniuxiaoan@MacBook-Pro.local>
2020-01-02 10:38:42 +0800

    update

--- prometheus/pull.go
+++ prometheus/pull.go
@@ -5,25 +5,35 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"log"
+	"time"
 )
 
 //经典的pull模式, 适合long running 或者对监控实时性要求不是特别高的场景。
 
 var pullCounterVec *prometheus.CounterVec
+var pullHistogramVec *prometheus.HistogramVec
 
 func init() {
 	pullCounterVec = prometheus.NewCounterVec(prometheus.CounterOpts{
-		Name: "request_total",
-		Help: "request_total",
+		Name: "http_request_total",
+		Help: "http_request_total",
 	}, []string{"method","path"})
+
+	pullHistogramVec = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Name:        "http_request_duration",
+		Help:        "http_request_duration",
+	}, []string{"method", "path"})
 
 	//将此metric信息注册进Gather中，这样在提供metrics接口时就可以
 	//按照prometheus的方式将metric展现出来了
 	prometheus.MustRegister(pullCounterVec)
+	prometheus.MustRegister(pullHistogramVec)
 }
 
-func pullMiddleware(fn echo.HandlerFunc) echo.HandlerFunc {
+func Middleware(fn echo.HandlerFunc) echo.HandlerFunc {
 	return func(ctx echo.Context) error {
+		now := time.Now()
+
 		defer func() {
 			if err := recover(); err != nil {
 				log.Printf("middleware panic: %v", err)
@@ -35,13 +45,18 @@ func pullMiddleware(fn echo.HandlerFunc) echo.HandlerFunc {
 			"method": ctx.Request().Method,
 		}).Add(1)
 
+		pullHistogramVec.With(prometheus.Labels{
+			"path": ctx.Path(),
+			"method": ctx.Request().Method,
+		}).Observe(float64(time.Since(now).Milliseconds()))
+
 		return fn(ctx)
 	}
 }
 
 func Pull() {
 	svc := echo.New()
-	svc.Use(pullMiddleware)
+	svc.Use(Middleware)
 	svc.GET("/metrics", echo.WrapHandler(promhttp.Handler()))
 	svc.GET("/test1", TestHandler)
 	svc.GET("/test2", TestHandler)
--- prometheus/push.go
+++ prometheus/push.go
@@ -16,16 +16,26 @@ func TestHandler(ctx echo.Context) error {
 }
 
 var counterVec *prometheus.CounterVec
+var histogramVec *prometheus.HistogramVec
 
 func init() {
 	//注意第二个参数,里面指定的label在Add的时候必须要体现到，否则会panic
 	counterVec = prometheus.NewCounterVec(prometheus.CounterOpts{
 		Name: "http_request_total",
 		ConstLabels: prometheus.Labels{
-			"svcName": "echo",
+			"svcName": "echo-test",
 		},
 		Help:"request total",
 	}, []string{"path","method"})
+
+	histogramVec = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Name:        "http_request_duration",
+		Help:        "http_request_duration",
+		ConstLabels: prometheus.Labels{
+			"svcName": "echo-test",
+		},
+		Buckets:[]float64{10, 100, 500, 1000},
+	}, []string{"path", "method"})
 
 	go func() {
 		defer func() {
@@ -37,12 +47,17 @@ func init() {
 
 		// docker pull prom/pushgateway
 		// docker run -it -p 9091:9091 prom/pushgateway
-		pusher := push.New("http://127.0.0.1:9091", "http_request").Collector(counterVec)
+		httpRequestPusher := push.New("http://127.0.0.1:9091", "http_request").Collector(counterVec)
+		httpRequestDurationPusher := push.New("http://127.0.0.1:9091", "http_request_duration").Collector(histogramVec)
 		for {
 			select {
 			case <-timer.C:
-				if err := pusher.Push(); err != nil {
-					log.Printf("push metri error: %v", err)
+				if err := httpRequestPusher.Push(); err != nil {
+					log.Printf("push metric error: %v", err)
+				}
+
+				if err := httpRequestDurationPusher.Push(); err != nil {
+					log.Panicf("push duration metric error: %v", err)
 				}
 				timer.Reset(time.Second * 5)
 			}
@@ -52,6 +67,8 @@ func init() {
 
 func PrometheusMiddleware(fn echo.HandlerFunc) echo.HandlerFunc {
 	return func(ctx echo.Context) error {
+		now := time.Now()
+
 		defer func() {
 			if err := recover(); err != nil {
 				log.Printf("middleware panic: %v", err)
@@ -62,6 +79,12 @@ func PrometheusMiddleware(fn echo.HandlerFunc) echo.HandlerFunc {
 			"path":ctx.Path(),
 			"method": ctx.Request().Method,
 		}).Add(1)
+
+		histogramVec.With(prometheus.Labels{
+			"path": ctx.Path(),
+			"method": ctx.Request().Method,
+		}).Observe(float64(time.Since(now).Milliseconds()))
+
 
 		return fn(ctx)
 	}
